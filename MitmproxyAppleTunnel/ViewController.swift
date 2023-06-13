a7b9d67b710e8e4adbfb215ffcd58494c120ede9
emanuele-em <micheletti.emanuele@hotmail.com>
2023-06-13 09:48:59 +0200

    Initial Commit

---
+++ MitmproxyAppleTunnel/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MitmproxyAppleTunnel
+//
+//  Created by Emanuele Micheletti on 13/06/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
